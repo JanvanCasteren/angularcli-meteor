58c102974a17d93ad00038b5242036b65f33243a
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-09-11 17:07:12 +0200

    Initialize client app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chats, Messages } from 'api/collections';
+import { Chat } from 'api/models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  chats = "Cannot reach Meteor backend";
+
+  constructor() {}
+
+  ngOnInit() {
+    Chats.find({}).subscribe((chats) => {
+      console.log(chats);
+      this.chats = chats;
+    });
+  }
 }
